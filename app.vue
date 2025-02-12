56d37ef01f38a66f8c32e2b532a9baafe8286a84
yogyy <yogimr261@gmail.com>
2025-02-12 20:19:43 +0700

    intial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script lang="ts"></script>
+
+<template>
+  <NuxtLayout>
+    <div
+      class="min-h-dvh overflow-hidden sm:mt-14 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+      <NuxtPage />
+    </div>
+  </NuxtLayout>
+</template>
