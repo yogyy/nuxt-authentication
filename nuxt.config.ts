e91ea0aaa6c060c95a1d15fb7cdd967a6ab5b415
yogyy <yogimr261@gmail.com>
2025-02-13 23:24:42 +0700

    feat: add tailwind + shadcn(vue)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxthub/core"],
-  css: ["~/assets/css/main.css"],
+  modules: [
+    "@nuxtjs/color-mode",
+    "@nuxthub/core",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+  ],
   hub: { database: true },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  colorMode: {
+    classSuffix: "",
+  },
 });
